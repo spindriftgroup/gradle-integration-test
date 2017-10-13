bebd7f4c574eeba777e3e328f7c7966f136e7582
hallatech <warren@spindriftgroup.com>
2017-10-13 14:49:36 +0900

    Add additional commentary and licensing

--- src/main/groovy/com/spindrift/gradle/plugins/IntegrationTestPluginExtension.groovy
+++ src/main/groovy/com/spindrift/gradle/plugins/IntegrationTestPluginExtension.groovy
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2012-2017 Spindrift B.V. All Rights Reserved
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.spindrift.gradle.plugins
 /**
  * Provides configuration for the integration test plugin
@@ -16,11 +25,27 @@ package com.spindrift.gradle.plugins
 
 class IntegrationTestPluginExtension {
 
+  /**
+   * Configures and overrides
+   *   sourceSets {
+   *     integrationTest {
+   *       java.srcDir
+   *     }
+   *   }
+   */
   String javaSourceDir = 'src/integration-test/java'
   void javaSourceDir(String javaSourceDir) {
     this.javaSourceDir = javaSourceDir
   }
 
+  /**
+   * Configures and overrides
+   *   sourceSets {
+   *     integrationTest {
+   *       resources.srcDir
+   *     }
+   *   }
+   */
   String resourcesSourceDir = 'src/integration-test/resources'
   void resourcesSourceDir(String resourcesSourceDir) {
     this.resourcesSourceDir = resourcesSourceDir
